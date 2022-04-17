728a5ab336ababc33696db7030f250be97b9ac45
Dang Vinh <chubuc2411@gmail.com>
2022-04-17 21:19:07 +0700

    fix bug

--- src/main/java/com/backend/ecom/entities/Discount.java
+++ src/main/java/com/backend/ecom/entities/Discount.java
@@ -29,6 +29,10 @@ public class Discount {
 
     private LocalDate endDate;
 
+    @OneToMany(mappedBy = "discount")
+    @JsonIgnore
+    private Set<Product> products = new HashSet<>();
+
    public Discount(DiscountRequestDTO discountRequestDTO){
        this.percentage = discountRequestDTO.getPercentage();
        this.startDate = discountRequestDTO.getStartDate();
