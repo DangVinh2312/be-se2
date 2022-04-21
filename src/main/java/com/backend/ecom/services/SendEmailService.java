1e2bd224443e14d7f2735c1fdb58c54398508e9e
Dang Vinh <chubuc2411@gmail.com>
2022-04-21 11:27:38 +0700

    update mail

--- src/main/java/com/backend/ecom/services/SendEmailService.java
+++ src/main/java/com/backend/ecom/services/SendEmailService.java
@@ -1,21 +1,27 @@
 package com.backend.ecom.services;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class SendEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendEmail(String to, String body, String topic){
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("DiGi_World");
-        simpleMailMessage.setTo(to);
-        simpleMailMessage.setSubject(topic);
-        simpleMailMessage.setText(body);
-        javaMailSender.send(simpleMailMessage);
+    @SneakyThrows
+    public void sendEmail(String to, String body, String topic) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setFrom(new InternetAddress("chubuc2411@gmail.com", "DiGi World"));
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(topic);
+        mimeMessageHelper.setText(body);
+        javaMailSender.send(mimeMessage);
     }
 }
