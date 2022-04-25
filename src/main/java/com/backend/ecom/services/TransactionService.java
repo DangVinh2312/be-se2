4f62fad28be90d13126af96b4906b6d71b33ede9
VuDoiMu <98081088+VuDoiMu@users.noreply.github.com>
2022-04-25 21:13:25 +0700

    Add files via upload

--- src/main/java/com/backend/ecom/services/TransactionService.java
+++ src/main/java/com/backend/ecom/services/TransactionService.java
@@ -1,16 +1,19 @@
 package com.backend.ecom.services;
 
-import com.backend.ecom.entities.Brand;
-import com.backend.ecom.entities.Transaction;
+import com.backend.ecom.entities.*;
 import com.backend.ecom.exception.ResourceNotFoundException;
 import com.backend.ecom.payload.response.ResponseObject;
 import com.backend.ecom.repositories.TransactionRepository;
+import com.backend.ecom.supporters.PaymentType;
+import com.backend.ecom.supporters.TransactionStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class TransactionService {
@@ -29,8 +32,15 @@ public class TransactionService {
     public ResponseEntity<ResponseObject> updateTransaction(Long id, Transaction transactionRequest) {
         Transaction transaction = transactionRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Not found brand with id:" + id));
-//        transaction.setName(transactionRequest.getName());
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok", "Delete transaction successfully", ""));
+
+        transaction.setUser(transactionRequest.getUser());
+        transaction.setPaymentType(transactionRequest.getPaymentType());
+        transaction.setStatus(transactionRequest.getStatus());
+        transaction.setVoucher(transactionRequest.getVoucher());
+        transaction.setTotalPrice(transactionRequest.getTotalPrice());
+        transaction.setCart(transactionRequest.getCart());
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok", "Delete transaction successfully", transactionRepository.save(transaction)));
     }
 
     public ResponseEntity<ResponseObject> deleteTransaction(Long id) {
@@ -40,6 +50,29 @@ public class TransactionService {
         transactionRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok", "Delete transaction successfully", ""));
 
+    }
+
+    public ResponseEntity<ResponseObject> createTransaction(Cart cart, Voucher appliedVoucher, String message, PaymentType paymentType, TransactionStatus transactionStatus){
+        Set<CartItem>  cartItem = cart.getCartItems();
+        double price = 0;
+        for (CartItem item : cartItem){
+            price = price + item.getTotalPrice();
+        }
+
+        price = Voucher.applyVoucher(price, appliedVoucher);
+
+        com.backend.ecom.entities.User user = cart.getUser();
+
+        Transaction transaction = new Transaction(user, paymentType,transactionStatus, message, appliedVoucher, price, cart);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseObject("ok", "Transaction created!", transaction));
+    }
+
+    public ResponseEntity<ResponseObject> updatePayment(Long id, TransactionStatus transactionStatus){
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found brand with id:" + id));
+        transaction.setStatus(transactionStatus);
+        transactionRepository.save(transaction);
+        return ResponseEntity.ok( new ResponseObject("ok", "Transaction status updated!", transaction));
     }
 
 }
